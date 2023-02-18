51f768d9b1b31e9544c2f5d40df90cc83e784783
Shantanu Nawade <nawadekaustubh@gmail.com>
2023-02-18 16:26:05 +0530

    added-stats

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -1,23 +1,50 @@
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image, Link } from '@chakra-ui/react'
 import React from 'react'
 import GitHubCalendar from "react-github-calendar";
 
 const Github = () => {
   return (
     <>
-     <Heading mb="2rem" textAlign={"center"}>Github Stats</Heading>
+     <Heading marginTop="30px" mb="2rem" textAlign={"center"}>Github Stats</Heading>
      <Box>
-        <Box width="1000px" margin="auto">
+        <Box width="870px" margin="auto" marginBottom="30px">
         <GitHubCalendar
             username="KaustubhN12"
             className="react-activity-calendar"
           />
         </Box>
-        <Flex>
-            <Box>
-                
-            </Box>
-        </Flex>
+        <Flex
+        flexDir={{ base: "column", lg: "row" }}
+        margin={"1rem"}
+        align={"center"}
+        justify={"center"}
+        gap="7"
+      >
+        <Box>
+          <Link href="https://github.com/KaustubhN12" />
+          <Image
+            id="github-streak-stats"
+            width="500px"
+            src="https://github-readme-streak-stats.herokuapp.com?user=KaustubhN12&theme=dark"
+          />
+        </Box>
+        <Box>
+          <Link href="https://github.com/KaustubhN12" />
+          <Image
+            id="github-top-langs"
+            width="500px"
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=KaustubhN12&layout=compact&theme=dark"
+          />
+        </Box>
+        <Box>
+          <Link href="https://github.com/KaustubhN12" />
+          <Image
+            width="500px"
+            id="github-stats-card"
+            src="https://github-readme-stats.vercel.app/api?username=KaustubhN12&count_private=true&show_icons=true&theme=dark"
+          />
+        </Box>
+      </Flex>
      </Box>
     </>
   )
